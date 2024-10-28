1265a1b0d17cc1ba73ec8a5e8bd20cdb79372ac3
Ruben Rubio Perucha <rubenru94@hotmail.com>
2024-10-28 17:20:14 +0100

    feat(express): Set config handling cors

--- express/src/core/app.ts
+++ express/src/core/app.ts
@@ -1,5 +1,6 @@
+import cors from 'cors';
 import { config } from 'dotenv';
-import express from 'express';
+import express, { json, text, urlencoded } from 'express';
 import httpContext from 'express-http-context';
 import { connectToDatabase } from './config/db.config';
 
@@ -8,19 +9,20 @@ config();
 class AppConfig {
   private app: express.Express | undefined;
 
-  public async bootstrap(): Promise<express.Express> {
+  public async start(): Promise<express.Express> {
     try {
       this.app = express();
       this.app.use(httpContext.middleware);
 
       /**
+       * @info config
+       */
+      this.config();
+
+      /**
        * @info Database connection
        */
       connectToDatabase();
-
-      /**
-       * @info config
-       */
 
       /**
        * @info error handling
@@ -31,6 +33,14 @@ class AppConfig {
       throw error;
     }
   }
+
+  private config(): void {
+    this.app.use(json({ limit: '1mb' }));
+    this.app.use(text({ type: 'text/html' }));
+    this.app.use(cors());
+    this.app.use(urlencoded({ extended: false }));
+    this.app.disable('x-powered-by');
+  }
 }
 
-export default new AppConfig().bootstrap();
+export default new AppConfig().start();
